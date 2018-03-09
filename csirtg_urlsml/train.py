332aff07f13774c778319cf5a1396c426533c183
Wes Young <wes@barely3am.com>
2018-03-08 19:36:18 -0500

    travis, updating sklearn

--- csirtg_urlsml/train.py
+++ csirtg_urlsml/train.py
@@ -110,7 +110,7 @@ def main():
 
     if args.indicator:
         feats = _extract_features(args.indicator)
-        feats = np.array(feats, dtype=int)
+        feats = np.array([feats], dtype=int)
         p = classifier.predict(feats)
         print(p)
 
