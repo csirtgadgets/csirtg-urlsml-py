9c1c89d518ec4975f5f17c96f635db248a259196
Wes Young <wes@barely3am.com>
2018-04-06 08:47:41 -0400

    fixes

--- csirtg_urlsml/__init__.py
+++ csirtg_urlsml/__init__.py
@@ -5,12 +5,24 @@ import pickle
 from csirtg_urlsml.url import predict as predict_url
 from pprint import pprint
 from .constants import PYVERSION
+import sys
+
 
 me = os.path.dirname(__file__)
 
 MODEL = os.getenv('CSIRTG_URLSML_MODEL', '%s/../data/model.pickle' % me)
 if PYVERSION == 2:
     MODEL = os.getenv('CSIRTG_URLSML_MODEL', '%s/../data/py2model.pickle' % me)
+
+
+MODEL = 'model.pickle'
+if PYVERSION == 2:
+    MODEL = 'py2model.pickle'
+
+if os.path.exists(os.path.join(sys.prefix, 'csirtg_urlsml/data', MODEL)):
+    MODEL = os.path.join(sys.prefix, 'csirtg_urlsml/data', MODEL)
+else:
+    MODEL = os.path.join('%s/../data/%s' % (os.path.dirname(__file__), MODEL))
 
 CLS = None
 if os.path.exists(MODEL):
